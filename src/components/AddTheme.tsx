5e5267aab68b2991abb95f07c85cf8daf1c2c9d8
unknown <Hassan Ali>
2024-11-18 13:54:46 +0500

    fixing issue

--- src/components/AddTheme.tsx
+++ src/components/AddTheme.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { SketchPicker } from 'react-color'; 
 import Color from '../../public/images/colors.png'
 import Image from 'next/image';
